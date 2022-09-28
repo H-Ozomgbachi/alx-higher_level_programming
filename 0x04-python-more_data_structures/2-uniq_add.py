7498d6910b6724f05aa49f9592da305f1975a12c
H-Ozomgbachi <ozomgbachih@gmail.com>
2022-09-28 23:23:17 +0000

    fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
+	"""
+	adds all unique integers in a list
+	Args:
+		my_list - given list
+	Return:
+		Data - sum
+	"""
+
 	x = list(set(my_list))
 	return sum(x)
