04f573e4310a4701e4122a074d92ab79ea2e0730
H-Ozomgbachi <ozomgbachih@gmail.com>
2022-09-29 01:19:28 +0000

    Task 10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+	if not a_dictionary:
+		return None
 	key_list = list(a_dictionary.keys())
 	max_val = max(a_dictionary.values())
 	max_pos = list(a_dictionary.values()).index(max_val)
